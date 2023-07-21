661c7c9cffd80e1366eb38e732af5abac174a9b7
bautistamarcelo <bautista.marceloricardo@gmail.com>
2023-07-20 23:02:40 -0300

    Hasta primer ejercicio de la E2

---
+++ etapa_02/Ejercicios/Ejercicio1.py
@@ -0,0 +1,6 @@
+#Creamos una variable para contener el valor a evaluar
+age = int(input("Ingrese su edad en Números por favor: "))
+if age >= 18:
+    print(f'Su edad es de {age} años. Es Ud. mayor de edad')
+else:
+    print(f'Su edad es de {age} años. Ud. es menor')
