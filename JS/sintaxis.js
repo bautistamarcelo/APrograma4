b491276e3c4af55dbdf7bd019929a17df77935e1
bautistamarcelo <bautista.marceloricardo@gmail.com>
2023-09-28 12:48:24 -0300

    AP4.0 Bootstrap terminado

---
+++ JS/sintaxis.js
@@ -0,0 +1,19 @@
+let x = 'uva';
+
+switch (x) {
+    case 'Manzana':
+        console.log('agrupando la ' +  x + ' con Manzanas');
+        break;
+    case 'Manzana':
+        console.log('agrupando la ' + x + ' con Manzanas');
+        break;
+    case 'Manzana':
+        console.log('agrupando la ' + x + ' con Manzanas');
+        break;
+    default:
+        console.log('La ' + x + ' es una fruta nueva')
+        break;
+}
+
+let clientes = ['Marcelo', 'Cecilia', 'Ana', 'Guadalupe', 'Paula']
+console.log(clientes)
---
+++ JS/tipos_datos.js
@@ -0,0 +1,26 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+//Los números extra grandes o extra pequeños se pueden escribir con notación científica (exponencial):
+let y = 123e5;    // 12300000
+let z = 123e-5;   // 0.00123
+
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2023-09-07");
+console.log(date)
